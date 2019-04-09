ebe76344b0b6e542e3f0a1167be479acd66907d4
Igor Protsyuk <batko_zeus@i.ua>
2019-04-09 13:31:12 +0300

    change draggble logic

--- src/task.jsx
+++ src/task.jsx
@@ -4,11 +4,19 @@ import { Draggable } from 'react-beautiful-dnd';
 
 
 const Container = styled.div`
+    display: flex;
     border: 1px solid lightgrey;
     border-radius: 2px;
     padding: 8px;
     margin-bottom: 8px;
     background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
+`;
+const Handle = styled.div`
+    border-radius: 4px;
+    width: 20px;
+    height: 20px;
+    background-color: orange;
+    margin-right: 8px;
 `;
 
 export default class Column extends React.Component {
@@ -21,10 +29,10 @@ export default class Column extends React.Component {
                 {(provided, snapshot) => (
                     <Container
                         {...provided.draggableProps}
-                        {...provided.dragHandleProps}
                         ref={provided.innerRef}
                         isDragging={snapshot.isDragging}
                     >
+                        <Handle {...provided.dragHandleProps} />
                         {this.props.task.content}
                     </Container>
                 )}
